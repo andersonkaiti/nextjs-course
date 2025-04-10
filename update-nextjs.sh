1549985d91f7bbd457383e719a884ce24d48ed65
andersonkaiti <anderkaiti@gmail.com>
2025-04-10 08:51:34 -0300

    :wrench: chore: Atualizando o Next.js para a versão 15.3.0 e incluindo o Turbopack no script de desenvolvimento.

---
+++ update-nextjs.sh
@@ -0,0 +1,7 @@
+cd authentication-demo && yes | npx @next/codemod@canary upgrade latest && cd ..
+cd data-fetching-demo && yes | npx @next/codemod@canary upgrade latest && cd ..
+cd hello-world && yes | npx @next/codemod@canary upgrade latest && cd ..
+cd multiple-root-layouts && yes | npx @next/codemod@canary upgrade latest && cd ..
+cd rendering-demo && yes | npx @next/codemod@canary upgrade latest && cd ..
+cd route-handlers-demo && yes | npx @next/codemod@canary upgrade latest && cd ..
+cd routing-demo && yes | npx @next/codemod@canary upgrade latest && cd ..
